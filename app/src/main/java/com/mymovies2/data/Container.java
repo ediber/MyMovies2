89bc373737b2be4bc95991203653c224a7e0afc9
edib <edibersh@gmail.com>
2020-01-28 15:34:24 +0200

    Container.java - integrated Realm

--- app/src/main/java/com/mymovies2/data/Container.java
+++ app/src/main/java/com/mymovies2/data/Container.java
@@ -7,14 +7,15 @@ import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmList;
+import io.realm.RealmResults;
 
 class Container {
 
-    private  Realm realm;
-    private List<IMovieHeadline> headlines;
+    private Realm realm;
+    private RealmResults<Movie> headlines;
 
     public Container(Context context) {
-      //  this.headlines = new ArrayList<>();
+        //  this.headlines = new ArrayList<>();
 
         // Initialize Realm (just once per application)
         Realm.init(context);
@@ -22,24 +23,30 @@ class Container {
 // Get a Realm instance for this thread
         realm = Realm.getDefaultInstance();
 
-        this.headlines = (List<IMovieHeadline>) realm.where(Movie.class);
-        if(headlines == null){
-            headlines = new RealmList();
-        }
+        headlines = realm.where(Movie.class).findAll();
+
+
     }
 
     public void setHeadlines(List<IMovieHeadline> headlines) {
-        this.headlines = headlines;
+        for (IMovieHeadline headline : headlines) {
+            realm.beginTransaction();
+            realm.copyToRealm((Movie) headline);
+            realm.commitTransaction();
+        }
+        this.headlines = realm.where(Movie.class).findAll();
     }
 
     public void changeSelected(IMovieHeadline headline) {
         for (IMovieHeadline myHeadline : headlines) {
-            if(myHeadline.getId().equals(headline.getId())){
-                if(myHeadline.getIsSelected()){
+            if (myHeadline.getId().equals(headline.getId())) {
+                realm.beginTransaction();
+                if (myHeadline.getIsSelected()) {
                     myHeadline.setIsSelected(false);
                 } else { // not selected
                     myHeadline.setIsSelected(true);
                 }
+                realm.commitTransaction();
             }
         }
     }
@@ -48,20 +55,27 @@ class Container {
         List<IMovieHeadline> selected = new ArrayList<>();
 
         for (IMovieHeadline headline : headlines)
-        if(headline.getIsSelected()){
-            selected.add(headline);
-        }
+            if (headline.getIsSelected()) {
+                selected.add(headline);
+            }
+
         return selected;
     }
 
     public List<IMovieHeadline> getMoviesHeadlines() {
-        return headlines;
+        List<IMovieHeadline> simpleHeadlines = new ArrayList<>();
+
+        for (IMovieHeadline headline : headlines) {
+            simpleHeadlines.add(headline);
+        }
+
+        return simpleHeadlines;
     }
 
     public Movie getMovieById(String id) {
         for (IMovieHeadline headline : headlines) {
-            if(headline.getId().equals(id)){
-                return  (Movie)headline;
+            if (headline.getId().equals(id)) {
+                return (Movie) headline;
             }
         }
         return null;
